62b9eed45b6cecb70a10ad86447b3805edcdfbd4
Stefan Ziegler <edi.gonzales@gmail.com>
2021-01-24 16:43:53 +0100

    initial commit

---
+++ lib/src/test/java/ch/so/agi/ili2db/libnative/Ili2dbTest.java
@@ -0,0 +1,17 @@
+package ch.so.agi.ili2db.libnative;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Ili2dbTest {
+    @Test
+    public void test_ili2db_Ok() {
+        Ili2db library = new Ili2db();
+    }
+    
+    
+//    void testSomeLibraryMethod() {
+//        Library classUnderTest = new Library();
+//        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+//    }
+}
